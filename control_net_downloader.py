327dc844e10e9a7ae6a7a6dcdb2aaa244d0a7097
Dmitry <mospost83@yandex.ru>
2024-06-09 14:14:25 +0300

    обновил файл control_net_downloader

--- control_net_downloader.py
+++ control_net_downloader.py
@@ -58,7 +58,8 @@ urls = [
 	"https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus-face_sdxl_vit-h.safetensors",
 	"https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors",
 	"https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sdxl.bin",
-	"https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15.bin"	
+	"https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15.bin",
+	"https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl.safetensors"	
 ]
 
 # Create the target folder if it doesn't exist
